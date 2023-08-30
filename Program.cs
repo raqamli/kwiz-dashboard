448a28216cd7a6c29ab45ab6de2f05cfd6c28d49
Wahid Abduhakimov <wakhid2802@gmail.com>
2023-08-30 14:53:19 +0900

    Remove bin/obj

--- Program.cs
+++ Program.cs
@@ -6,6 +6,22 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
+builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri("http://localhost:5204"))
+    .AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
+builder.Services.AddTransient(sp => 
+   sp.GetRequiredService<IHttpClientFactory>().CreateClient("ServerAPI"));
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.MetadataUrl = "https://auth.xloyiha.tech/realms/xloyiha/.well-known/openid-configuration";
+    options.ProviderOptions.Authority = "https://auth.xloyiha.tech/realms/xloyiha";
+    options.ProviderOptions.ClientId = "dashboard";
+    options.ProviderOptions.ResponseType = "id_token token";
+
+    options.UserOptions.NameClaim = "preferred_username";
+    options.UserOptions.RoleClaim = "roles";
+    options.UserOptions.ScopeClaim = "scope";
+});
 
 await builder.Build().RunAsync();
