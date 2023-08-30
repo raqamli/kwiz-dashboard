a6c9528b6c35f63cf6ca90a1cdf31f9bc037784a
Mehriddin <71074521+mekhriddinbozorov@users.noreply.github.com>
2023-08-30 12:17:33 +0500

    CU-860rn083a - Fix Kwiz Dashboard namespace and project name (#2)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using kwiz_dashboard;
+using Kwiz.Dashboard;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
