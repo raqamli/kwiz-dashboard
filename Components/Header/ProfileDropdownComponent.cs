9f8b416b571feacf17e72c0707a32d6967ede352
sarvar-swe <s.azodov.software.engineer@gmail.com>
2023-09-06 01:23:26 +0500

    CU-860rn0rmj - Profile dropdown

--- Components/Header/ProfileDropdownComponent.cs
+++ Components/Header/ProfileDropdownComponent.cs
@@ -9,21 +9,18 @@ public partial class ProfileDropdownComponent : ComponentBase
 {
     private AuthenticationState authState;
     private string ProfileUrl() 
-    => $"{Configuration.GetValue<string>("Oidc:Keycloak:RealmUrl")}/account/#/personal-profile";
+        => $"{Configuration.GetValue<string>("Oidc:Keycloak:RealmUrl")}/account/#/personal-profile";
     [Inject] private AuthenticationStateProvider AuthStateProvider { get; set; }
     [Inject] private IConfiguration Configuration { get; set; }
     [Inject] private NavigationManager Navigation { get; set; }
-    [Inject] SignOutSessionStateManager SignOutManager { get; set; }
-
     protected override async Task OnInitializedAsync()
     {
-        var authState = await AuthStateProvider.GetAuthenticationStateAsync();
+        authState = await AuthStateProvider.GetAuthenticationStateAsync();
         await base.OnInitializedAsync();
     }
 
     private void BeginSignOut(MouseEventArgs args)
     {
-        Navigation.NavigateTo("authentication/logout");
-        Navigation.NavigateToLogout("authentication/logout");
+        Navigation.NavigateToLogout("https://auth.xloyiha.tech/realms/xloyiha/protocol/openid-connect/logout");
     }
 }
