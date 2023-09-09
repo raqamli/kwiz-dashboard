0fee9faa809e4cd05826e0cd63ef64f427084598
tparizoda <parizodatogaeva@gmail.com>
2023-09-09 07:24:57 -0700

    ModalPageIndexRazor

--- Shared/MainLayout.cs
+++ Shared/MainLayout.cs
@@ -7,18 +7,26 @@ namespace Kwiz.Dashboard.Shared;
 
 public partial class MainLayout : LayoutComponentBase
 {
-    private bool UserInterestsSaved = false;
+    // private bool UserInterestsSaved = false;
 
+    // [Inject]
+    // public IHttpClientFactory ClientFactory { get; set; }
+    // protected override async Task OnInitializedAsync()
+    // {
+    //     var client = ClientFactory.CreateClient("ServerAPI");
+    //     var response = await client.GetAsync("api/v1/userinfo/interests");
+    //     if(response.StatusCode is System.Net.HttpStatusCode.NoContent)
+    //         UserInterestsSaved = false;
+
+    //     await base.OnInitializedAsync();
+    // }
+    private IEnumerable<UserInterest> Interests { get; set; }
+    
     [Inject]
-    public IHttpClientFactory ClientFactory { get; set; }
+    public IKwizApiHttpClient KwizApiHttpClient { get; set; }
     protected override async Task OnInitializedAsync()
     {
-        var client = ClientFactory.CreateClient("ServerAPI");
-        var response = await client.GetAsync("api/v1/userinfo/interests");
-        if(response.StatusCode is System.Net.HttpStatusCode.NoContent)
-            UserInterestsSaved = false;
-
-        await base.OnInitializedAsync();
+        Interests = (await KwizApiHttpClient.GetUserInterestsAsync()).ToList();
     }
 }
 
